887d1abeacf399023a214a0093d6d4783b5bbd57
Juan García <superjuan.madrid2015@gmail.com>
2023-08-29 21:34:33 +0200

    finish basic without extra pages

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-// Fetch the techstack information
+// Fetch the skills
 fetch('skills.json')
     .then(response => response.json())
     .then(data => {
-        const technologiesSection = document.getElementById('technologies');
+        const technologiesSection = document.getElementById('technologies')
 
         for (const property in data) {
-            const h3 = document.createElement('h3');
-            h3.textContent = property.charAt(0).toUpperCase() + property.slice(1);
+            const h3 = document.createElement('h3')
+            h3.textContent = property.charAt(0).toUpperCase() + property.slice(1)
 
-            const divContainer = document.createElement('div');
-            divContainer.classList.add(property);
+            const divContainer = document.createElement('div')
+            divContainer.classList.add(property)
 
-            technologiesSection.appendChild(h3);
-            technologiesSection.appendChild(divContainer);
+            technologiesSection.appendChild(h3)
+            technologiesSection.appendChild(divContainer)
 
             if (Array.isArray(data[property])) {
                 data[property].forEach(item => {
-                    const div = document.createElement('div');
+                    const div = document.createElement('div')
 
-                    const icon = document.createElement('i');
-                    icon.classList.add(item.iconType, item.icon);
+                    const icon = document.createElement('i')
+                    icon.classList.add(item.iconType, item.icon)
 
-                    const p = document.createElement('p');
-                    p.textContent = item.name.charAt(0).toUpperCase() + item.name.slice(1);;
+                    const p = document.createElement('p')
+                    p.textContent = item.name.charAt(0).toUpperCase() + item.name.slice(1)
 
-                    div.appendChild(icon);
-                    div.appendChild(p);
+                    div.appendChild(icon)
+                    div.appendChild(p)
 
-                    divContainer.appendChild(div);
-                });
+                    divContainer.appendChild(div)
+                })
             }
 
             const skillCss = `#technologies .${property} {
@@ -36,7 +36,7 @@ fetch('skills.json')
                 justify-content: flex-start;
                 align-items: flex-start;
                 flex-wrap: wrap;
-            }`;
+            }`
             const technologiesContainerCss = `#technologies .${property} div {
                 display: flex;
                 justify-content: flex-start;
@@ -51,9 +51,44 @@ fetch('skills.json')
                 border: rgb(38, 38, 38) solid 1px;
                 border-radius: 0.375rem;
                 overflow: hidden;
-            }`;
-            const style = document.createElement('style');
-            style.innerText = technologiesContainerCss + skillCss;
+            }`
+            const style = document.createElement('style')
+            style.innerText = technologiesContainerCss + skillCss
             document.head.appendChild(style)
         }
-    });
+    })
+
+// Fetch the skills
+fetch('posts.json')
+    .then(response => response.json())
+    .then(data => {
+        const postsSection = document.getElementById('posts')
+
+        for (const post of data.posts) {
+
+            const aTag = document.createElement('a')
+            //aTag.href = post.url
+            aTag.target = "_blank"
+            postsSection.appendChild(aTag)
+
+            const articleContainer = document.createElement('article')
+            aTag.appendChild(articleContainer)
+
+            const img = document.createElement('img')
+            img.src = "./blogimages/" + post.img
+
+            articleContainer.appendChild(img)
+
+            const h3 = document.createElement('h3')
+            h3.textContent = post.title.charAt(0).toUpperCase() + post.title.slice(1)
+
+            articleContainer.appendChild(h3)
+
+            const p = document.createElement('p');
+            const maxLength = 150;
+            const shortDescription = post.description.length > maxLength ? post.description.substring(0, maxLength) + "..." : post.description;
+            p.textContent = shortDescription;
+
+            articleContainer.appendChild(p)
+        }
+    })
