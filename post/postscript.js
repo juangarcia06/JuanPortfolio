e528a1beea01e3bf7530e9340a6ae735fe73917f
Juan García <superjuan.madrid2015@gmail.com>
2023-08-31 13:40:33 +0200

    if no content dont add nothing

--- post/postscript.js
+++ post/postscript.js
@@ -23,28 +23,38 @@ fetch('../posts.json')
         const header = document.querySelector("h1");
         header.textContent = post.title;
 
-        const img = document.createElement('img')
-        img.src = "../blogimages/" + post.img
-        img.alt = (post.img.charAt(0).toUpperCase() + post.img.slice(1)).replace(/\.[^/.]+$/, "")
-        img.classList.add("titleimage")
-        img.loading = "lazy";
-        postsSection.appendChild(img)
+        if (post.img != "") {
+            const img = document.createElement('img')
+            img.src = "../blogimages/" + post.img
+            img.alt = (post.img.charAt(0).toUpperCase() + post.img.slice(1)).replace(/\.[^/.]+$/, "")
+            img.classList.add("titleimage")
+            img.loading = "lazy";
+            postsSection.appendChild(img)
+        }
 
-        const p = document.createElement('p')
-        p.innerText = post.description
-        postsSection.appendChild(p)
+        if (post.description != "") {
+            const p = document.createElement('p')
+            p.innerText = post.description
+            postsSection.appendChild(p)
+        }
 
         for (const comment of post.comments) {
-            const h2 = document.createElement('h2')
-            h2.textContent = comment.title.charAt(0).toUpperCase() + comment.title.slice(1)
-            postsSection.appendChild(h2)
+            if (comment.title != "") {
+                const h2 = document.createElement('h2')
+                h2.textContent = comment.title.charAt(0).toUpperCase() + comment.title.slice(1)
+                postsSection.appendChild(h2)
+            }
 
-            const img = document.createElement('img')
-            img.src = "../blogimages/" + comment.img
-            postsSection.appendChild(img)
+            if (comment.img != "") {
+                const img = document.createElement('img')
+                img.src = "../blogimages/" + comment.img
+                postsSection.appendChild(img)
+            }
 
-            const p = document.createElement('p');
-            p.textContent = comment.comment;
-            postsSection.appendChild(p)
+            if (comment.comment != "") {
+                const p = document.createElement('p');
+                p.textContent = comment.comment;
+                postsSection.appendChild(p)
+            }
         }
     })
